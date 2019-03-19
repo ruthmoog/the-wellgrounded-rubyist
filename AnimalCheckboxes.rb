7a04f38fbb9be8bab567f2da4e56ab6c34cd17a3
Ruth Baker <ruthmoog@gmail.com>
2019-03-19 21:09:43 +0000

    Refractored to iterate over items per check list, and same for returning the selected results.

--- AnimalCheckboxes.rb
+++ AnimalCheckboxes.rb
@@ -1,32 +1,32 @@
 require 'sinatra'
 
+checklist = {
+  "this is a cat": "<p>purrrrrrrrrrr. </p>",
+  "this is a bear": "<p><em>ragghghrrhrhrhr!</em>  i r a tired bear. </p>",
+  "this is a squirrel": "<p>nip nip, i have a fluffy tail. </p>"
+}
+
+questions_markup = "<p>Animal To Do List:</p>"
+checklist.each do | key , value |
+    questions_markup += "<label><input name='#{key}' type='checkbox'>click me if #{key}</label><br />"
+end 
+
+
 get '/' do
-  '<h1> Hello, pepper</h1>
-  <form method="post">
-    <label><input name="check1" type="checkbox">click me for cats</label><br />
-    <label><input name="check2" type="checkbox">click me for bears</label><br />
-    <label><input name="check3" type="checkbox">click me for squirrels</label><br />
-
-
-    <input type="submit" value="Submit!">
-
+  "<h1>Animal Checkboxes</h1>
+  <form method='post'>
+  #{questions_markup}
+    <input type='submit' value='Submit!'>
   </form>
-  '
+  "
 end
-
-cattext = "purrrrrrrrrrr. "
-beartext = "ragghghrrhrhrhr!  I r a tired bear. "
-squirreltext = "nip nip, i have a fluffy tail. "
 
 post '/' do
   animalstring = ""
-  if params[:check1] == "on"
-      animalstring += cattext
+  checklist.each do | key, value|
+    if params[key] == "on"
+      animalstring += value
+    end
   end
-  if params[:check2] == "on"
-      animalstring += beartext
-  end
-  if params[:check3] == "on"
-      animalstring += squirreltext
-  end
+  animalstring
 end
