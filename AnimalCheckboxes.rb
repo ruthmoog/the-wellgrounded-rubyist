7957b4238368824236e2386f8c4f5718398037fb
Ruth Baker <ruthmoog@gmail.com>
2019-03-20 16:25:38 +0000

    Program now parses animal checklist and it's data from a csv file

--- AnimalCheckboxes.rb
+++ AnimalCheckboxes.rb
@@ -1,14 +1,18 @@
 require 'sinatra'
+require 'csv'
 
-checklist = {
-  "this is a cat": "<p>purrrrrrrrrrr. </p>",
-  "this is a bear": "<p><em>ragghghrrhrhrhr!</em>  i r a tired bear. </p>",
-  "this is a squirrel": "<p>nip nip, i have a fluffy tail. </p>"
-}
+my_file = "Animal_Checkboxes.csv"
+
+# checklist = {
+#   "this is a cat": "<p>purrrrrrrrrrr. </p>",
+#   "this is a bear": "<p><em>ragghghrrhrhrhr!</em>  i r a tired bear. </p>",
+#   "this is a squirrel": "<p>nip nip, i have a fluffy tail. </p>"
+# }
 
 questions_markup = "<p>Animal To Do List:</p>"
-checklist.each do | key , value |
-    questions_markup += "<label><input name='#{key}' type='checkbox'>click me if #{key}</label><br />"
+CSV.foreach(my_file) do | row |
+# checklist.each do | key , value |
+    questions_markup += "<label><input name='#{row[0]}' type='checkbox'>click me if #{row[0]}</label><br />"
 end 
 
 
@@ -23,9 +27,10 @@ end
 
 post '/' do
   animalstring = ""
-  checklist.each do | key, value|
-    if params[key] == "on"
-      animalstring += value
+  CSV.foreach(my_file) do | row |
+  # checklist.each do | key, value|
+    if params[row[0]] == "on"
+      animalstring += row[1]
     end
   end
   animalstring
