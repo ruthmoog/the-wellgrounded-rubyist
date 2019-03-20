08857584bdfd832bc896e304200deee06ef1ee33
Ruth Baker <ruthmoog@gmail.com>
2019-03-20 14:00:16 +0000

    Converting method to temp class the converts c to f & vice versa

--- wellgroundedruby/c2f.rb
+++ wellgroundedruby/c2f.rb
@@ -1,5 +1,9 @@
-celsius = 100
-fahrenheit = (celsius * 9 / 5) + 32
-print "The result is: "
-print fahrenheit
-puts "."
+class Temperature
+    def Temperature.ctf(celsius)
+        celsius * 9.0 / 5 + 32
+    end
+
+    def Temperature.ftc(fahrenheit)
+        (fahrenheit - 32) * 5 / 9.0
+    end
+end
