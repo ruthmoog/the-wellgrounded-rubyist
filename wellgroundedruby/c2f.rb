59ebfc779a815a0e999e38183a25881bbfae3330
Ruth Baker <ruthmoog@gmail.com>
2019-03-20 14:20:37 +0000

    This class automatically converts from c to f & vice versa and puts the result on instance creation

--- wellgroundedruby/c2f.rb
+++ wellgroundedruby/c2f.rb
@@ -1,9 +1,17 @@
 class Temperature
-    def Temperature.ctf(celsius)
-        celsius * 9.0 / 5 + 32
-    end
 
-    def Temperature.ftc(fahrenheit)
-        (fahrenheit - 32) * 5 / 9.0
+    def initialize(measure,meter)
+        @mea = measure
+        @met = meter
+
+        if @met.downcase.start_with?("c")
+            puts @mea * 9.0 / 5 + 32
+        else 
+            puts (@mea - 32) * 5 / 9.0
+        end
     end
 end
+
+
+puts this = Temperature.new(100,"C")
+puts that = Temperature.new(100,"F")
